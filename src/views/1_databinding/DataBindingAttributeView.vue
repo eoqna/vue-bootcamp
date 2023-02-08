adb4ac4de471fdf4d098b571950dd8a0a72f71e4
eoqna <eoqna@naver.com>
2023-02-08 22:59:00 +0900

    [eoqna] 23-02-08 commit

---
+++ src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" :value="userId" readonly />
+    <input type="text" :value="userId" readonly />
+    <br>
+    <img :src="imgSrc" alt="" style="width:100px; height:auto;" />
+    <br>
+    <input type="search" v-model="txt1" />
+    <button :disabled="txt1 === ''" >조회</button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'daebeom.go',
+      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
+      txt1: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello, {{ userName }}</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      msg: 'Welcome 개발자의 품격',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
